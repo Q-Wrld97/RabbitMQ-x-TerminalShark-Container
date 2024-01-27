dde4ba1842f239c0e48cf778f7256d2e87ebab11
Q-wrld97 <dough_boy97@hotmail.com>
2024-01-26 21:31:29 -0500

    File restructure

---
+++ DockerFile/ConsumerDemo/image_receiver.py
@@ -0,0 +1,41 @@
+import pika
+from pika.exchange_type import ExchangeType
+import bson
+
+def consumer_connection(routing_key):
+    # Establish a connection to RabbitMQ server
+    connection_parameters = pika.ConnectionParameters('localhost')
+    connection = pika.BlockingConnection(connection_parameters)
+
+    # Create a channel
+    channel = connection.channel()
+
+    # Declare a queue (queue names are generated based on the routing key)
+    queue_name = routing_key
+
+    # Consume messages from the queue
+    a=channel.basic_consume(queue=queue_name, auto_ack=True,
+        on_message_callback=on_message_received)
+    
+    print('Preprocess Starting Consuming')
+    
+    try:
+        channel.start_consuming()
+    except KeyboardInterrupt:
+        channel.close()
+        connection.close()
+
+def on_message_received(ch, method, properties, body):
+    #body=bson.loads(body)
+    #save the image
+    with open('DockerFile/Consumer/x.png', 'wb') as image_file:
+        image_file.write(body)
+    body=bson.loads(body)
+    print(f"""
+            received new message: {body}
+            routing key: {method.routing_key}
+            properties: {properties.delivery_mode}
+            channel: {ch}""")
+    
+
+consumer_connection('Dashboard')
